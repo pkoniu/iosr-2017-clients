ec26ff58641fdf4e0306260887df2281b6e8b7e7
Patryk Konior <patryk@arteia.co>
2018-01-02 21:34:09 +0100

    trying to make push to dockerhub work v2

--- .travis/push_dockerhub.sh
+++ .travis/push_dockerhub.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-docker login -u $DOCKER_USER -p $DOCKER_PASS
+echo "$DOCKER_PASS"
+docker login -u "$DOCKER_USER" -p "$DOCKER_PASS"
 
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     TAG="latest"
