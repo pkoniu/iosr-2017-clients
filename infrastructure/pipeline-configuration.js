9c42b50f551f19e8c5cb8404d0f9e5fb7213970f
Patryk Konior <patryk@arteia.co>
2018-01-03 23:43:11 +0100

    changes to travis deploymeny

---
+++ infrastructure/pipeline-configuration.js
@@ -0,0 +1,12 @@
+const {
+    HEROKU_APP_NAME = 'iosr2017clients'
+} = process.env;
+
+module.exports = {
+    name: 'iosr2017-clients-pipeline',
+    apps: {
+        staging: `${HEROKU_APP_NAME}-staging`,
+        production: `${HEROKU_APP_NAME}-production`
+    },
+    owner: 'patryk.konior@gmail.com'
+};
