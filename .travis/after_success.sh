12d3df61378bf1aca073c56ab033f003f944515f
Patryk Konior <patryk@arteia.co>
2018-01-04 12:27:27 +0100

    generalizing travis deployment - fix2

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-heroku pipelines:promote -a $(HEROKU_APP_NAME)-staging -r master -t $(HEROKU_APP_NAME)-production
+heroku pipelines:promote -a ${HEROKU_APP_NAME}-staging -r master -t ${HEROKU_APP_NAME}-production
